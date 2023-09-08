f6f8b624f8da46191e36588656028f1ebd761d18
Mohammed Sajeed <115149642+gitofsajeed@users.noreply.github.com>
2023-09-08 17:41:06 +0530

    Update profile.component.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,14 +8,18 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit{
-  constructor(private service : FormService, private route: ActivatedRoute){}
+  constructor(
+    private service : FormService,
+    private route: ActivatedRoute){}
 
   formData : any; 
   // imgURL= this.service.imageUrl
   image : any = null
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
+    this.route.params.
+      subscribe(params => 
+        {
       const index = +params['index'];
       this.formData = this.service.serviceArray[index];
       // this.imgURL = this.service.serviceArray[index].image
